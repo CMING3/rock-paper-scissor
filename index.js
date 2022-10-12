c255ec889d6a309cfdbff283dcf16e32862f3f36
CMING <cming.pms@gmail.com>
2022-10-12 23:42:59 +0800

    tidy up the code, clear some rubbish

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const handsArr = ["Rock", "Paper", "Scissor"]
-let playerchoice = "";
-let computerchoice = "";
+let playerSelection = "";
+let computerSelection = "";
+const message = document.querySelector(".winningmessage")
 
-
+function getComputerSelection(){
+    computerSelection = handsArr[Math.floor(Math.random()*handsArr.length)];
+    document.querySelector(".cpuchoice").innerHTML = computerSelection;
+}
 
 document.querySelectorAll(".hand").forEach(element=>{
-    element.addEventListener("click", startGame);
+    element.addEventListener("click", (e)=>{
+        playerSelection = e.target.innerText;
+        getComputerSelection()
+        playRound(playerSelection)
+    });
 })
 
-function startGame(e){
-    playerchoice = e.target.innerText;
-    const message = document.querySelector(".winningmessage")
-    document.querySelector(".yourchoice").innerHTML = playerchoice;
-    getComputerChoice(playerchoice)
-    if (playerchoice===computerchoice){
+
+
+function playRound(){
+    document.querySelector(".yourchoice").innerHTML = playerSelection;
+    //Game rule
+    if (playerSelection===computerSelection){
+
         message.innerHTML = "Draw"
-    }else if (playerchoice=="Rock"&&computerchoice=="Paper"){
-        message.innerHTML = "Paper beat Rock, You Lose !";
-    }else if (playerchoice=="Rock"&&computerchoice=="Scissor"){
-        message.innerHTML = "Rock beat Scissor, You Win !";
-    }else if (playerchoice=="Paper"&&computerchoice=="Rock"){
-        message.innerHTML = "Paper beat Rock, You Win !";
-    }else if (playerchoice=="Paper"&&computerchoice=="Scissor"){
-        message.innerHTML = "Scissor beat Paper, You Lose !";
-    }else if (playerchoice=="Scissor"&&computerchoice=="Rock"){
-        message.innerHTML = "Rock beat Scissor, You Lose !";
-    }else if (playerchoice=="Scissor"&&computerchoice=="Paper"){
-        message.innerHTML = "Scissor beat Paper, You Win !";
+
+    }else if ((playerSelection=="Scissor"&&computerSelection=="Paper")||
+            (playerSelection=="Rock"&&computerSelection=="Scissor")||
+            (playerSelection=="Paper"&&computerSelection=="Rock")){
+
+        message.innerHTML = `${playerSelection} beat ${computerSelection}, You Win !`;
+
+    }else if ((playerSelection=="Paper"&&computerSelection=="Scissor")||
+            (playerSelection=="Rock"&&computerSelection=="Paper")||
+            (playerSelection=="Scissor"&&computerSelection=="Rock")){
+
+        message.innerHTML = `${computerSelection} beat ${playerSelection}, You Lose !`;
     }
 }
 
-function getComputerChoice(playerchoice){
-    computerchoice = handsArr[Math.floor(Math.random()*handsArr.length)];
-    document.querySelector(".cpuchoice").innerHTML = computerchoice;
-}
